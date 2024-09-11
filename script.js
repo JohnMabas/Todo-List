d7bac85770c1100cfafabbd93380cbbbbce44973
JohnMabas <johnmabas1@gmail.com>
2024-09-11 08:00:56 +0100

    updata a function

--- script.js
+++ script.js
@@ -12,26 +12,20 @@ renderTodoList();
 
 let todoListHtml = '';
 
-for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
+todoList.forEach(function(todoObject, index) {
     const {name, dueDate} = todoObject;
-
     const html = `
     <div>${name}</div> 
     <div>${dueDate}</div>
     <button onClick="
-        todoList.splice(${i}, 1);
+        todoList.splice(${index}, 1);
         renderTodoList();
     " class="delete-todo-button">Delete</button> 
 
     `;
     todoListHtml += html;
 
-}
-
-
+});
 
 document.querySelector('.js-todo-list')
 .innerHTML = todoListHtml;
